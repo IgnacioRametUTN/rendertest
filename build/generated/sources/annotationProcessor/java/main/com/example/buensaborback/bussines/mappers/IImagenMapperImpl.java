5cac2d19fbe01752a0786c8d674a78753672e8c9
IgnacioRametUTN <ignacio.ramet@alumnos.frm.utn.edu.ar>
2024-06-05 12:43:29 -0300

    ignore clean

---
+++ build/generated/sources/annotationProcessor/java/main/com/example/buensaborback/bussines/mappers/IImagenMapperImpl.java
@@ -0,0 +1,13 @@
+package com.example.buensaborback.bussines.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-30T19:18:28-0300",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 19.0.2 (Oracle Corporation)"
+)
+@Component
+public class IImagenMapperImpl implements IImagenMapper {
+}
